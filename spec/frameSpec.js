cc954460416fb5424ec4c386fcba1069e0bc9962
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-04 15:04:48 +0100

    added test for frame class to start with default of 0 for both rolls, fails test

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,1 +1,14 @@
 'use strict';
+
+describe('Frame', function () {
+  var frame;
+
+  beforeEach(function () {
+    frame = new Frame();
+  });
+
+  it("starts with 0 for both rolls", function () {
+    expect(frame.rollOne).toEqual(0)
+    expect(frame.rollTwo).toEqual(0);
+  })
+});
